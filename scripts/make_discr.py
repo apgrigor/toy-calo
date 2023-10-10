81eaa725163281464fcaf90aa6fefc9ce4e96818
apgrigor <apollongregoras@gmail.com>
2023-10-10 12:14:55 +0300

    new dataset scheduling script

---
+++ scripts/make_discr.py
@@ -0,0 +1,52 @@
+import argparse
+import subprocess
+import numpy as np
+import pprint as pp
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-r", "--reso", action = "store_true")
+parser.add_argument("-e", "--energy", action = "store_true")
+
+args = parser.parse_args()
+
+def total_reso(reso, noise, const, energy):
+    return 100 * np.sqrt(
+        reso / energy + noise / energy**2 + const
+    )
+
+default_reso = 0.05 ** 2
+default_noise = 0.5 ** 2
+constant_term = 0.0055 ** 2
+
+sqrt10 = np.sqrt(10)
+
+params = [
+    (default_reso * (sqrt10 ** order), default_noise * (sqrt10 ** order))
+    for order in range(-5, 7)
+]
+
+total_resos = [
+    (i - 5, total_reso(reso, noise, constant_term, 10))
+    for i, (reso, noise) in enumerate(params)
+]
+
+pp.pprint(total_resos)
+
+energies = [5, 10, 20, 50, 100]
+
+if args.reso:
+    print("making distinct resolution samples")
+    for reso, noise in params:
+        subprocess.run([
+            "xmake", "run", "toy", "flat", "discr",
+            str(reso), str(noise)
+        ])
+
+if args.energy:
+    print("making energy samples")
+    for energy in energies:
+        subprocess.run([
+            "xmake", "run", "toy", "exact", "discr",
+            str(default_reso), str(default_noise), str(energy)
+        ])
